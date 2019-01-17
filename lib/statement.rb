2ac11505b27963ae0870f578a156c55fd364bbb1
mateuszstacel <mateusz.stacel92@gmail.com>
2019-01-17 09:29:00 +0000

    move responsibility of statement class from account class

--- lib/statement.rb
+++ lib/statement.rb
@@ -12,11 +12,17 @@ class Statement
   def transactions_statement
     print "Date || Credit || Debit || Balance\n"
     @statement.reverse_each do |x|
-      print "#{x[0]} || #{x[1]} || #{x[2]} || #{x[3]}\n"
+      print "#{x[0]} || #{statement_format(x[1])} || #{statement_format(x[2])} || #{statement_format(x[3])}\n"
     end
   end
 
+private
+
   def statement_format(amount)
+    if amount == ""
+      return ""
+    else
     '%.2f' % amount
+    end
   end
 end
